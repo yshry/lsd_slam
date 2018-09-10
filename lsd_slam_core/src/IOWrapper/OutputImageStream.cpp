c95dc6431464de62f87e911344f664683d1abf80
yshry <yasuhiro.yao@gmail.com>
2018-09-10 23:55:22 +0900

    commit

--- lsd_slam_core/src/IOWrapper/OutputImageStream.cpp
+++ lsd_slam_core/src/IOWrapper/OutputImageStream.cpp
@@ -1,2 +1,29 @@
 #include "OutputImageStream.h"
 
+namespace lsd_slam
+{
+OutputImageStream::OutputImageStream(const std::__cxx11::string& topic_name)
+: image_transporter(nh_)
+{
+  std::string local_topic_name = "lsd_slam/" + topic_name;
+  //image_transport::ImageTransport it(nh_);
+  image_channel = nh_.resolveName(local_topic_name);
+  image_publisher = image_transporter.advertise(image_channel, 1);
+  
+}
+
+void OutputImageStream::publishImage(const cv::Mat& cv_image)
+{
+  cv_bridge::CvImagePtr cv_ptr;
+  try
+  {
+    //cv_ptr = cv_bridge::
+  }
+  catch (cv_bridge::Exception e)
+  {
+  }
+
+}
+
+  
+}
--- lsd_slam_core/src/IOWrapper/OutputImageStream.h
+++ lsd_slam_core/src/IOWrapper/OutputImageStream.h
@@ -1,5 +1,8 @@
 #include <ros/ros.h>
 #include <opencv2/opencv.hpp>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/Image.h>
 
 namespace lsd_slam
 {
@@ -10,10 +13,13 @@ public:
     OutputImageStream(const std::string& topic_name);
     ~OutputImageStream();
 
-    void imageCb(const cv::Mat& cv_image);
+    void publishImage(const cv::Mat& cv_image);
 
 private:
     ros::NodeHandle nh_;
+    std::string image_channel;
+    image_transport::Publisher image_publisher;
+    image_transport::ImageTransport image_transporter;
 
 };
 
